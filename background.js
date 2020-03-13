ad117f04adac801342b65e83c934156d0f354eea
Luis Alvarez <laar0716@outlook.com>
2020-03-12 21:56:11 -0600

    Modify content scripts match

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab) {
-      if (changeInfo.status == 'complete' || changeInfo.url) {
+      if (changeInfo.status == 'complete') {
         chrome.tabs.sendMessage( tabId, {
           message: 'add_claim',
           url: changeInfo.url
